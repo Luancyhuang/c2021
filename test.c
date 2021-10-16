04e1f0fab65fcfa47e9c9810dcd4370893cad3df
Luancyhuang <542914825@qq.com>
2021-10-16 12:59:14 +0800

    ##构建新符号

--- test.c
+++ test.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
-//编写一个函数getbits，从一个16位的单元中取出某几位（即该几位保留原值，其余位为0）。函数调用形式为getbits(value n1,n2).
-//value 为该16位（2个字节）中的数据值，n1为欲取出的起始位，n2为欲取出的结束位，例如：
-//getbits(0101675,5,8)表示对八进制101675这个数，取出它的从左边起第5位到第8位
-unsigned short getbits(unsigned short value, int n1,int n2)
-{
-  unsigned short z = 0;
-  z = ~0;
-  z = (z >> n1) & (z << (16-n2));
-  z = value & z;
-  z = z >> (16-n2);
-  return z;
-}
+//#define XNAME(n) student##n
+#define CONT(base,n) base##e##n
 int main()
 {
-  unsigned short a = 0;
-  int n1 = 0;
-  int n2 = 0;
-  printf("input an octal number: && n1: n2:");
-  scanf("%o %d %d",&a,&n1,&n2);
-  unsigned short ret = getbits(a,n1,n2);
-  printf("ret= %o\n",ret);
+  printf("%lf\n",CONT(3.14,2));
+ // XNAME(1);
+ // XNAME(2);
+ // XNAME(3);
+ // XNAME(4);
+ // XNAME(5);
   return 0;
 }
+  
