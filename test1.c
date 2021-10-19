6c7528b2d2fbfde82cd9b0069f2512c0af390952
Luancyhuang <542914825@qq.com>
2021-10-19 15:24:19 +0800

    函数指针调用的练习

--- test1.c
+++ test1.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+void welcome()
+{
+  printf("###############\n");
+  printf("#1.play 2.exit#\n");
+  printf("###############\n");
+}
+void getgift()
+{
+  printf("恭喜你获得黄金大礼包一份\n");
+
+}
+void login(void (*welcome)(),void (*getgift)())	
+{
+#define NAME "小学生"
+#define PASSWD "12345"
+  char name[32] = {0};
+  char passwd[32] = {0};
+  printf("Please Enter Your Name#");
+  scanf("%s",name);
+  printf("Please Enter Your Passwd#");
+  scanf("%s",passwd);
+  if(strcmp(name,NAME) == 0 && strcmp(passwd,PASSWD) == 0)
+  {
+    welcome();
+    getgift();
+  }
+}
 int main()
 {
-  int a[4] = {1,2,3,4};
-  int *ptr1 = (int *)(&a+1);
-  int *ptr2 = (int *)((int)a+1);
-  printf("%x,%x\n",ptr1[-1],*ptr2);
+  login(welcome,getgift);
   return 0;
 }
