871c2d9e2e239fabf71baac5b88e87a3c97e2b9c
Luancyhuang <542914825@qq.com>
2021-10-21 17:19:18 +0800

    可变参数列表

--- test1.c
+++ test1.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
-#include <string.h>
-void welcome()
+int find_max_data(int num,...)
 {
-  printf("###############\n");
-  printf("#1.play 2.exit#\n");
-  printf("###############\n");
-}
-void getgift()
-{
-  printf("恭喜你获得黄金大礼包一份\n");
-
-}
-void login(void (*welcome)(),void (*getgift)())	
-{
-#define NAME "小学生"
-#define PASSWD "12345"
-  char name[32] = {0};
-  char passwd[32] = {0};
-  printf("Please Enter Your Name#");
-  scanf("%s",name);
-  printf("Please Enter Your Passwd#");
-  scanf("%s",passwd);
-  if(strcmp(name,NAME) == 0 && strcmp(passwd,PASSWD) == 0)
+  va_list arg;
+  va_start(arg,num);
+  int max = va_arg(arg,int);
+  
+  for(int i=1;i<num;i++)
   {
-    welcome();
-    getgift();
+    int curr = va_arg(arg,int);
+    if(max<curr)
+    {
+      max = curr;
+    }  
   }
+  
+  va_end(arg);
+  return max;
 }
+
 int main()
 {
-  login(welcome,getgift);
+  int max = find_max_data(5,11,22,33,44,55);
+  printf("max=%d\n",max);
+
   return 0;
 }
