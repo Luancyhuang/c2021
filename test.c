a41fe8685d4b949061a8fb96022dec3f6902ac54
Luancyhuang <542914825@qq.com>
2021-10-27 20:25:11 -0700

    移除单链表指定元素

--- test.c
+++ test.c
@@ -1,51 +1,65 @@
 #include <stdio.h>
-//给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
-//进阶：尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
+#include <stdlib.h>
 
-void reverse(int *nums,int left,int right)//对一段区间进行逆置
+struct listnode
 {
-  while(left<right)
-  {
-    int tmp = nums[left];
-    nums[left] = nums[right];
-    nums[right] = tmp;
-    left++;
-    right--;//逆置算法
-  }
-}
+  int val;
+  struct listnode* next;
+};
 
-void rotate(int *nums,int numsSize,int k)
-{  
-  //1.如果k==numsSize时，数组旋转回来到原点;2.如果k>numsSize时，数组旋转又到到k的下标处。
-  if(k >= numsSize)
-    k %= numsSize;
-  //前n-k个数逆置
-  reverse(nums,0,numsSize-k-1);
-  //后k个数逆置
-  reverse(nums,numsSize-k,numsSize-1);
-  //整体逆置
-  reverse(nums,0,numsSize-1);
-}
-
-void print(int *arr,int num)
+struct listnode* removeelements(struct listnode* head,int val)
 {
-  for(int i=0;i<10;i++)
+  struct listnode *prev = NULL,*cur = head;
+  while(cur)
   {
-    printf("%d ",arr[i]);
+    if(cur->val == val)
+    {
+      //头删
+      if(cur == head)
+      {
+        head = cur->next;
+	free(cur);
+	cur = head;
+      }
+      //中间删
+      else
+      {
+        //停下来，删除
+        prev->next = cur->next;
+        free(cur);
+        cur = prev->next;
+      }
+    }
+    else
+    {
+      //迭代往后走
+      prev = cur;
+      cur = cur->next;
+    }  
   }
-  printf("\n");
+  return head;
 }
 
 int main()
 {
-  int arr[10] = {0};
-
-  for(int i=0;i<10;i++)
-  {
-    scanf("%d",&arr[i]);
-  }
-
-  rotate(arr,10,3);
-  print(arr,10);
+  struct listnode* n1 = (struct listnode*)malloc(sizeof(struct listnode));    
+  struct listnode* n2 = (struct listnode*)malloc(sizeof(struct listnode));    
+  struct listnode* n3 = (struct listnode*)malloc(sizeof(struct listnode));    
+  struct listnode* n4 = (struct listnode*)malloc(sizeof(struct listnode));    
+  struct listnode* n5 = (struct listnode*)malloc(sizeof(struct listnode));     
+  n1->val = 7;
+  n2->val = 7;
+  n3->val = 7;
+  n4->val = 7;
+  n5->val = 7;
+  
+  n1->next = n2;
+  n2->next = n3;
+  n3->next = n4;
+  n4->next = n5;
+  n5->next = NULL;
+  
+  struct listnode* head = removeelements(n1,7);
+  
   return 0;
 }
