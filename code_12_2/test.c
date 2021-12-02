419e5610ed8f11d1ab9b67b3399aaa0309f34281
黄新武 <542914825@qq.com>
2021-12-02 16:22:03 +0800

    杨氏矩阵中查找一个数字

--- code_12_2/test.c
+++ code_12_2/test.c
@@ -131,13 +131,107 @@ void RotateStr(char* str,int k)
 }*/
 
 //旋转字符串
-int main()
+/*int main()
 {
   //旋转字符串说明字符串要被修改放在字符数组中
   char arr[] = "abcdef";
   RotateStr(arr,8);//旋转字符串
   printf("%s\n",arr);//打印旋转后的字符串
 
+  return 0;
+}*/
+
+//写一个函数，判断一个字符串是否是否是另一个字符串旋转而来
+//例如：给定s1 =AABCD和s2 = BCDAA，返回1
+//给定s1=abcd和s2=ACBD，返回0.
+//AABCD左旋一个字符得到ABCDA
+//AABCD左旋两个字符得到BCDAA
+//AABCD右旋一个字符得到DAABC
+
+//判断arr2是否为arr1旋转而来
+
+/*int IsRotateStr(char* arr1,char* arr2)
+{
+  int len = strlen(arr1);
+  for(int i=0;i<len;i++)
+  {	  
+    RotateStr(arr1,1);
+
+    if(strcmp(arr1,arr2) == 0)
+      return 1;    
+  }
+  return 0;
+}
+
+int main()
+{
+  char arr1[] = "AABCD";
+  char arr2[] = "BCDAA";
+  int ret = IsRotateStr(arr1,arr2);
+  
+  if(ret == 1)
+  {
+    printf("yes\n");    
+  }
+  else
+  {
+    printf("no\n");  
+  }
+
+  return 0;
+}*/
+
+
+/*int main()
+{
+  char arr1[20] = "abc";
+  strcat(arr1,arr1);
+  printf("%s\n",arr1);
+
+  return 0;
+}*/
+
+//杨氏矩阵
+//有一个数字矩阵，矩阵的每行从左到右是递增，
+//矩阵从上到下是递增，
+//请编写程序在这样的矩阵中查找某个数字是否存在。
+//要求:时间复杂度小于O(N)。
+
+//1 2 3
+//4 5 6
+//7 8 9
+void FindK(int arr[3][3],int k,int row,int col)
+{
+  int x = 0;
+  int y = col - 1;
+  
+  while(x < row && y >= 0)
+  {	  
+    if(arr[x][y] < k)
+    {
+      x++;
+    } 
+    else if(arr[x][y] > k)
+    {
+      y--;
+    }
+    else
+    {
+      printf("找到了,下标是:%d %d\n",x,y);
+      return;//找到了就直接返回
+    }
+  }
+
+  printf("找不到\n");
+}
+//返回型参数- 用指针带回
+int main()
+{
+  int arr[3][3] = {1,2,3,4,5,6,7,8,9};
+  int k = 17;
+
+  FindK(arr,k,3,3);
+  
   return 0;
 }
 
