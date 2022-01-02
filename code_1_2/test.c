bd1e0f088254663b31004df0a904d6905913b277
黄新武 <542914825@qq.com>
2022-01-02 23:34:09 +0800

    指针数组

---
+++ code_1_2/test.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+/*int main()
+{
+  int a = 10;
+  int b = 20;
+  int c = 30;
+  int d = 40;
+
+  int* arr[4] = {&a,&b,&c,&d};
+  for(int i=0;i<4;i++)
+  {
+    printf("%d\n",*arr[i]);  
+  }
+  return 0;
+}*/
+
+/*int main()
+{
+  int arr1[] = {1,2,3,4,5};
+  int arr2[] = {2,3,4,5,6};
+  int arr3[] = {3,4,5,6,7};
+
+  int *parr[] = {arr1,arr2,arr3};
+  for(int i=0;i<3;i++)
+  {
+    for(int j=0;j<5;j++)
+    {
+      printf("%d ",parr[i][j]);
+    }
+    printf("\n");
+  }
+  return 0;
+}*/
+
+/*int main()
+{
+  const char* arr[] = {"abcde","bcdefa","hehe","haha"};
+  for(int i=0;i<5;i++)
+  {
+    printf("%s\n",arr[i]);
+  }
+
+  return 0;
+}*/
+
+
+/*int main()
+{
+  
+
+
+  return 0;
+}*/
+
+
+
+
+
